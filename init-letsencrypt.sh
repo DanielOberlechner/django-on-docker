1027db3c715808bfbf67d0c0a8fa41d42168051b
Daniel Oberlechner <danieloberlechner@hotmail.de>
2021-03-27 16:06:02 +0100

    almost there but a still have bugs in this project and I dont know any further :(

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,28 +7,30 @@ fi
 
 domains=(hok.ddns.net)
 rsa_key_size=4096
-data_path="./data/nginx/certbot"
+data_path="/etc/letsencrypt/certbot"
 email="hund@tirol.de" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
 fi
 
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
+  sudo mkdir -p "$data_path/conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
+
+
+
+
+
 echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
+path="/etc/letsencrypt/certbot/conf/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
@@ -38,16 +40,27 @@ docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
 echo
 
 
+
+
+
+
+
 echo "### Starting nginx ..."
 docker-compose -f docker-compose.prod.yml up --force-recreate -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
 docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/certbot/conf/live/$domains && \
+  rm -Rf /etc/letsencrypt/certbot/conf/archive/$domains && \
+  rm -Rf /etc/letsencrypt/certbot/conf/renewal/$domains.conf" certbot
 echo
+
+
+
+
+
+
 
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
@@ -65,6 +78,14 @@ esac
 
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+
+
+
+
+
+
+
 
 docker-compose -f docker-compose.prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
