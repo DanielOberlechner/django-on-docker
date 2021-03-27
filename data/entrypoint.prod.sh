dfe911d257a020af3d7b136be8d28fd2514a7a5c
Daniel Oberlechner <danieloberlechner@hotmail.de>
2021-03-27 13:55:49 +0100

    short backup before doing some serious stuff

--- data/entrypoint.prod.sh
+++ data/entrypoint.prod.sh
@@ -5,13 +5,10 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+      sleep 2.5
     done
 
     echo "PostgreSQL started"
 fi
-
-python manage.py flush --no-input
-python manage.py migrate
 
 exec "$@"
--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -5,10 +5,13 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+      sleep 2.5
     done
 
     echo "PostgreSQL started"
 fi
+
+python manage.py flush --no-input
+python manage.py migrate
 
 exec "$@"
